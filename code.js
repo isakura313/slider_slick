3bb1ecdd43f9b71fa553bd7c9b3de8cf86a8dcbd
Paul <isakura313@gmail.com>
2020-03-31 12:38:19 +0300

    add magnific

---
+++ code.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.img-gallery').slick({
+        dots: true,
+        infinite: true,
+        speed: 800,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    })
+});
